433580aeb629bedee75647bafc39a78200d30a53
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-06 14:09:02 +0100

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
